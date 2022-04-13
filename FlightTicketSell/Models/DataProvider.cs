916c49717d881f028879603ce261304658cbc3cd
Nguyen Van Hen <nguyenvanhen1502@gmail.com>
2022-04-13 13:57:41 +0700

    Add DataProvider

---
+++ FlightTicketSell/Models/DataProvider.cs
@@ -0,0 +1,40 @@
+﻿namespace FlightTicketSell.Models
+{
+    public class DataProvider
+    {
+        #region Private Members 
+
+        private static DataProvider _ins;
+
+        #endregion
+
+        #region Public Properties
+
+        public static DataProvider Ins
+        {
+            get
+            {
+                if (_ins == null)
+                    _ins = new DataProvider();
+                return _ins;
+            }
+            set
+            {
+                _ins = value;
+            }
+        }
+
+        public FlightTicketSellEntities DB { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        private DataProvider()
+        {
+            DB = new FlightTicketSellEntities();
+        }
+
+        #endregion
+    }
+}
