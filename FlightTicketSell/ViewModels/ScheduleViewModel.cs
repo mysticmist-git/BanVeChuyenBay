0ea48c9c7bf2bd96f64444aa492629dbea83ee3f
Gr33nBean <91081335+Gr33nBean@users.noreply.github.com>
2022-04-15 00:05:25 +0700

    Làm giao diện
    Thêm sân bay, thêm hạng vé, nhận lịch chuyến bay, nhập sân bay trung gian, nhập hạng vé

--- FlightTicketSell/ViewModels/ScheduleViewModel.cs
+++ FlightTicketSell/ViewModels/ScheduleViewModel.cs
@@ -1,7 +1,39 @@
-﻿namespace FlightTicketSell.ViewModels
+﻿using FlightTicketSell.Views;
+using System.Windows.Input;
+
+namespace FlightTicketSell.ViewModels
 {
     public class ScheduleViewModel : BaseViewModel
     {
+        #region Commands
+        public ICommand Open_Window_EnterLayoverAirport_Command { get; set; }
+        public ICommand Open_Window_EnterTicketClass_Command { get; set; }
+        #endregion
         public string Title { get; } = "NHẬN LỊCH";
+
+        public ScheduleViewModel()
+        {
+            Open_Window_EnterLayoverAirport_Command = new RelayCommand<object>(
+
+                (p) => { return true; },
+
+                (p) =>
+                {
+                    EnterLayoverAirportView enterLayoverAirportView = new EnterLayoverAirportView();
+                    enterLayoverAirportView.ShowDialog();
+                }
+            );
+
+            Open_Window_EnterTicketClass_Command = new RelayCommand<object>(
+
+                (p) => { return true; },
+
+                (p) =>
+                {
+                    EnterTicketClassView enterTicketClassView = new EnterTicketClassView();
+                    enterTicketClassView.ShowDialog();
+                }
+            );
+        }
     }
 }
--- FlightTicketSell/ViewModels/SettingViewModel.cs
+++ FlightTicketSell/ViewModels/SettingViewModel.cs
@@ -1,7 +1,40 @@
-﻿namespace FlightTicketSell.ViewModels
+﻿using FlightTicketSell.Views;
+using System.Windows.Input;
+
+namespace FlightTicketSell.ViewModels
 {
     public class SettingViewModel : BaseViewModel
     {
+        #region Commands
+        public ICommand Open_Window_MoreAirport_Command { get; set; }
+        public ICommand Open_Window_MoreTicketClass_Command { get; set; }
+        #endregion
+
         public string Title { get; } = "CÀI ĐẶT";
+
+        public SettingViewModel()
+        {
+            Open_Window_MoreAirport_Command = new RelayCommand<object>(
+
+                (p) => { return true; },
+
+                (p) =>
+                {
+                    MoreAirportView moreAirportView = new MoreAirportView();
+                    moreAirportView.ShowDialog();
+                }
+            );
+
+            Open_Window_MoreTicketClass_Command = new RelayCommand<object>(
+
+                (p) => { return true; },
+
+                (p) =>
+                {
+                    MoreTicketClassView moreTicketClassView = new MoreTicketClassView();
+                    moreTicketClassView.ShowDialog();
+                }
+            );
+        }
     }
 }
