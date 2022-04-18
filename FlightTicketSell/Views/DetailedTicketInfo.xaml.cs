6682cfec2c46f340b878db44f76f2ca0b4fe9324
nhattan040102-acc <tantran.040102@gmail.com>
2022-04-18 12:49:57 +0700

    update DetailedTicketInfo screen

---
+++ FlightTicketSell/Views/DetailedTicketInfo.xaml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FlightTicketSell.Views
+{
+    /// <summary>
+    /// Interaction logic for BookDetailView.xaml
+    /// </summary>
+    public partial class DetailedTicketInfo : UserControl
+    {
+        public BookDetailView()
+        {
+            InitializeComponent();
+        }
+    }
+}
