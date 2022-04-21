aed41a29bde7a98515df959f0fa1fd5eff62d585
Trần Nguyễn Nhật Tân <tantran.040102@gmail.com>
2022-04-21 22:09:38 +0700

    Add more model to Sell_Screen

---
+++ FlightTicketSell/Views/SellViewRelated/ChangeTicketView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FlightTicketSell.Views.SellViewRelated
+{
+    /// <summary>
+    /// Interaction logic for ChangeTicketView.xaml
+    /// </summary>
+    public partial class ChangeTicketView : UserControl
+    {
+        public ChangeTicketView()
+        {
+            InitializeComponent();
+        }
+    }
+}
