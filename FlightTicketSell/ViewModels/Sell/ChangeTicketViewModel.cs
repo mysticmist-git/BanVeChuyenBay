247d0aae348189287bc4504f888f3f6d70f2921e
Trần Nguyễn Nhật Tân <tantran.040102@gmail.com>
2022-05-07 10:00:40 +0700

    Load datagrid from database for ChangeTicket screen

--- FlightTicketSell/ViewModels/Sell/ChangeTicketViewModel.cs
+++ FlightTicketSell/ViewModels/Sell/ChangeTicketViewModel.cs
@@ -1,9 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FlightTicketSell.Views;
+using FlightTicketSell.Models;
+using FlightTicketSell.ViewModels.Sell;
+using FlightTicketSell.ViewModels.Search;
+using FlightTicketSell.Views.SearchViewMore;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
+using System.Linq;
+using System.Data.Entity.Core;
+using System.Windows;
+using System;
 
 namespace FlightTicketSell.ViewModels
 {
@@ -20,6 +25,14 @@ namespace FlightTicketSell.ViewModels
 
         public ICommand ReturnCommand { get; set; }
 
+        public ICommand LoadCommand { get; set; }
+        public DateTime TicketDeadline { get; set; }
+
+        public ObservableCollection<MidAirport_Search> MidAirports { get; set; }
+
+        public ObservableCollection<TicketSold> Customers { get; set; }
+
+        public string MaDuongBay { get; set; }
         #endregion
 
         #region Constructor
@@ -36,7 +49,47 @@ namespace FlightTicketSell.ViewModels
             //PrintTicketCommand = new RelayCommand<object>((p) => true, (p) => IoC.IoC.Get<ApplicationViewModel>().CurrentView = Models.AppView.);
 
             //CancelTicketCommand = new RelayCommand<object>((p) => true, (p) => IoC.IoC.Get<ApplicationViewModel>().CurrentView = Models.AppView.);
+
+            LoadCommand = new RelayCommand<object>((p) => true, (p) =>
+            {
+                using (var context = new FlightTicketSellEntities())
+                {
+                    try
+                    {
+                        MaDuongBay = context.CHUYENBAYs.Where(result => result.MaChuyenBay== 1).FirstOrDefault().MaDuongBay.ToString();
+
+                        MidAirports = new ObservableCollection<MidAirport_Search>(
+                                                                context.SANBAYTGs.Where(result =>
+                                                                result.MaDuongBay.ToString() == MaDuongBay)
+                                                                .Select(result => new MidAirport_Search
+                                                                {
+                                                                    ThuTu = result.ThuTu.ToString(),
+                                                                    TenSanBay = context.SANBAYs.Where(x => x.MaSanBay == result.MaSanBay).FirstOrDefault().TenSanBay,
+                                                                    GhiChu = result.GhiChu.ToString(),
+                                                                    ThoiGianDung = result.ThoiGianDung.ToString()
+                                                                }).ToList());
+
+                        Customers = new ObservableCollection<TicketSold>(
+                                                                context.VEs.Where(result =>
+                                                                result.MaChuyenBay == 1)
+                                                                .Select(result => new TicketSold
+                                                                {                                                                  
+                                                                    TenKhachHang = context.KHACHHANGs.Where(x => x.MaKhachHang == result.MaKhachHang).FirstOrDefault().HoTen,
+                                                                    MaKhachHang = result.MaKhachHang.ToString(),                                                                
+                                                                }).ToList()
+                                                           );
+
+                    }
+                    catch (EntityException)
+                    {
+                        // TODO: messagebox vo
+                        return;
+                    }
+                }
+            });
+
         }
+
 
         #endregion
 
--- FlightTicketSell/ViewModels/Sell/TicketInfoViewModel.cs
+++ FlightTicketSell/ViewModels/Sell/TicketInfoViewModel.cs
@@ -1,9 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FlightTicketSell.Views;
+using FlightTicketSell.Models;
+using FlightTicketSell.ViewModels.Sell;
+using FlightTicketSell.ViewModels.Search;
+using FlightTicketSell.Views.SearchViewMore;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
+using System.Linq;
+using System.Data.Entity.Core;
+using System.Windows;
+using System;
+
 
 namespace FlightTicketSell.ViewModels
 {
@@ -18,6 +24,20 @@ namespace FlightTicketSell.ViewModels
 
         public ICommand ReturnCommand { get; set; }
 
+        public ICommand LoadCommand { get; set; }
+
+        public ICommand HienGiaTien { get; set; }
+
+        public ObservableCollection<TicketClass> TicketClasses { get; set; }
+
+        public int MaChuyenBay { get; set; }
+
+        public int GiaTien { get; set; }
+
+        public TicketClass HangVe { get; set; }
+
+
+        public decimal HeSo {get; set; }
         #endregion
 
         #region Constructor
@@ -31,6 +51,58 @@ namespace FlightTicketSell.ViewModels
             ContinueCommand = new RelayCommand<object>((p) => true, (p) => IoC.IoC.Get<ApplicationViewModel>().CurrentView = Models.AppView.SellPay);
 
             ReturnCommand = new RelayCommand<object>((p) => true, (p) => IoC.IoC.Get<ApplicationViewModel>().CurrentView = Models.AppView.Sell);
+
+            LoadCommand = new RelayCommand<object>((p) => true, (p) =>
+            {
+                using (var context = new FlightTicketSellEntities())
+                {
+                    try
+                    {
+                        TicketClasses = new ObservableCollection<TicketClass>(context.CHUYENBAYs.Where(result =>
+                                                                result.MaChuyenBay == 1).FirstOrDefault()
+                                                                .CHITIETHANGVEs.Select(cthv => new TicketClass
+                                                                {
+                                                                    ID = cthv.MaHangVe,
+                                                                    Name = cthv.HANGVE.TenHangVe,
+                                                                    Price = 1000,
+                                                                }).ToList()); 
+                                                  
+
+                    }
+                    catch (EntityException)
+                    {
+                        // TODO: messagebox vo
+                        return;
+                    }
+                }
+            });
+
+            HienGiaTien = new RelayCommand<object>((p) => true, (p) =>
+            {
+            using (var context = new FlightTicketSellEntities())
+            {
+                try
+                {
+                        HeSo = context.HANGVEs.Where(result => result.TenHangVe == HangVe.Name).FirstOrDefault().HeSo;
+
+
+                        if (HangVe != null)
+                            GiaTien = System.Convert.ToInt32(context.CHUYENBAYs.Where(result =>
+                                                                result.MaChuyenBay == 1).FirstOrDefault()
+                                                                .GiaVe * HeSo);
+                                                                
+                      
+
+
+
+                    }
+                    catch (EntityException)
+                    {
+                        // TODO: messagebox vo
+                        return;
+                    }
+                }
+            });
         }
 
         #endregion
