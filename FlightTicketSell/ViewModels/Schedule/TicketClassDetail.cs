1d2c6e5488f8afb74da9cc66cfac5c890e85eecb
Gr33nBean <91081335+Gr33nBean@users.noreply.github.com>
2022-05-11 19:15:13 +0700

    Tiếp tục series code behind
    70% Phần thêm hạng vé trong ScheduleView

---
+++ FlightTicketSell/ViewModels/Schedule/TicketClassDetail.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightTicketSell.ViewModels.Schedule
+{
+    public class TicketClassDetail : BaseViewModel
+    {
+        #region Private Members
+
+        #endregion
+
+        #region Public Properties
+        public int Id { get; set; }
+        public int Id_TicketClass { get; set; }
+        public int Id_Flight { get; set; }
+        public string TicketClassName { get; set; }
+        public double TicketClassCoefficient { get; set; }
+        public double Prices { get; set; }
+        public int Seats { get; set; }
+        #endregion
+
+        #region Methods
+        #endregion
+    }
+}
