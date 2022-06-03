46c281fc2659f7bc7215380ba37caac4a7848095
Nguyen Van Hen <89691660+String1502@users.noreply.github.com>
2022-06-03 21:03:57 +0700

    [Query] - Update

--- SQL-Query/insert-data.sql
+++ SQL-Query/insert-data.sql
@@ -34,45 +34,45 @@ INSERT INTO SANBAY (TenSanBay, VietTat, TinhThanh, TrangThai) VALUES
 	(N'Phú Quốc', 'PQC', N'Kiên Giang', 1),
 	(N'Vân Đồn', 'VDO', N'Quảng Ninh', 1)
 
--- Nhập Đường bay
-INSERT INTO DUONGBAY (MaSanBayDen, MaSanBayDi, ThoiGianBay) VALUES (16, 1, 180)
-INSERT INTO DUONGBAY (MaSanBayDen, MaSanBayDi, ThoiGianBay) VALUES (1, 16, 200)
-INSERT INTO DUONGBAY (MaSanBayDen, MaSanBayDi, ThoiGianBay) VALUES (1, 14, 160)
+---- Nhập Đường bay
+--INSERT INTO DUONGBAY (MaSanBayDen, MaSanBayDi, ThoiGianBay) VALUES (16, 1, 180)
+--INSERT INTO DUONGBAY (MaSanBayDen, MaSanBayDi, ThoiGianBay) VALUES (1, 16, 200)
+--INSERT INTO DUONGBAY (MaSanBayDen, MaSanBayDi, ThoiGianBay) VALUES (1, 14, 160)
 
--- Nhập sân bay trung gian
-INSERT INTO SANBAYTG(MaDuongBay, MaSanBay, ThuTu, ThoiGianDung, GhiChu) VALUES (1, 5, 1, 13, N'Dừng cho vui')
-INSERT INTO SANBAYTG(MaDuongBay, MaSanBay, ThuTu, ThoiGianDung, GhiChu) VALUES (1, 7, 2, 14, N'Dừng cho vui x3')
+---- Nhập sân bay trung gian
+--INSERT INTO SANBAYTG(MaDuongBay, MaSanBay, ThuTu, ThoiGianDung, GhiChu) VALUES (1, 5, 1, 13, N'Dừng cho vui')
+--INSERT INTO SANBAYTG(MaDuongBay, MaSanBay, ThuTu, ThoiGianDung, GhiChu) VALUES (1, 7, 2, 14, N'Dừng cho vui x3')
 
-INSERT INTO SANBAYTG(MaDuongBay, MaSanBay, ThuTu, ThoiGianDung, GhiChu) VALUES (2, 8, 1, 19, N'Load hàng nóng')
+--INSERT INTO SANBAYTG(MaDuongBay, MaSanBay, ThuTu, ThoiGianDung, GhiChu) VALUES (2, 8, 1, 19, N'Load hàng nóng')
 
-INSERT INTO SANBAYTG(MaDuongBay, MaSanBay, ThuTu, ThoiGianDung, GhiChu) VALUES (3, 10, 1, 12, N'Cho khách đi WC?')
-INSERT INTO SANBAYTG(MaDuongBay, MaSanBay, ThuTu, ThoiGianDung, GhiChu) VALUES (3, 11, 2, 13, N'Cho khách mua đồ lưu niệm?')
+--INSERT INTO SANBAYTG(MaDuongBay, MaSanBay, ThuTu, ThoiGianDung, GhiChu) VALUES (3, 10, 1, 12, N'Cho khách đi WC?')
+--INSERT INTO SANBAYTG(MaDuongBay, MaSanBay, ThuTu, ThoiGianDung, GhiChu) VALUES (3, 11, 2, 13, N'Cho khách mua đồ lưu niệm?')
 
--- Nhập chuyến bay
-INSERT INTO CHUYENBAY (MaDuongBay, GiaVe, NgayGio) VALUES (1, 500000, CONVERT(DATETIME,'10/4/2022',103))
-INSERT INTO CHUYENBAY (MaDuongBay, GiaVe, NgayGio) VALUES (2, 620000, CONVERT(DATETIME,'11/4/2022',103))
-INSERT INTO CHUYENBAY (MaDuongBay, GiaVe, NgayGio) VALUES (3, 770000, CONVERT(DATETIME,'1/4/2022',103))
+---- Nhập chuyến bay
+--INSERT INTO CHUYENBAY (MaDuongBay, GiaVe, NgayGio) VALUES (1, 500000, CONVERT(DATETIME,'10/4/2022',103))
+--INSERT INTO CHUYENBAY (MaDuongBay, GiaVe, NgayGio) VALUES (2, 620000, CONVERT(DATETIME,'11/4/2022',103))
+--INSERT INTO CHUYENBAY (MaDuongBay, GiaVe, NgayGio) VALUES (3, 770000, CONVERT(DATETIME,'1/4/2022',103))
 
--- Nhập hạng vé
-INSERT INTO HANGVE (TenHangVe, HeSo, TrangThai) VALUES (N'Hạng Nhất', 1.6, 1)
-INSERT INTO HANGVE (TenHangVe, HeSo, TrangThai) VALUES (N'Thương gia', 1.4, 1)
-INSERT INTO HANGVE (TenHangVe, HeSo, TrangThai) VALUES (N'Phổ thông', 1.2, 1)
+---- Nhập hạng vé
+--INSERT INTO HANGVE (TenHangVe, HeSo, TrangThai) VALUES (N'Hạng Nhất', 1.6, 1)
+--INSERT INTO HANGVE (TenHangVe, HeSo, TrangThai) VALUES (N'Thương gia', 1.4, 1)
+--INSERT INTO HANGVE (TenHangVe, HeSo, TrangThai) VALUES (N'Phổ thông', 1.2, 1)
 
--- Nhập chi tiết hạng vé
+---- Nhập chi tiết hạng vé
 
-INSERT INTO CHITIETHANGVE (MaChuyenBay, MaHangVe, SoGhe) VALUES (1, 1, 10)
-INSERT INTO CHITIETHANGVE (MaChuyenBay, MaHangVe, SoGhe) VALUES (1, 2, 20)
-INSERT INTO CHITIETHANGVE (MaChuyenBay, MaHangVe, SoGhe) VALUES (1, 3, 60)
+--INSERT INTO CHITIETHANGVE (MaChuyenBay, MaHangVe, SoGhe) VALUES (1, 1, 10)
+--INSERT INTO CHITIETHANGVE (MaChuyenBay, MaHangVe, SoGhe) VALUES (1, 2, 20)
+--INSERT INTO CHITIETHANGVE (MaChuyenBay, MaHangVe, SoGhe) VALUES (1, 3, 60)
 
-INSERT INTO CHITIETHANGVE (MaChuyenBay, MaHangVe, SoGhe) VALUES (2, 1, 20)
-INSERT INTO CHITIETHANGVE (MaChuyenBay, MaHangVe, SoGhe) VALUES (2, 2, 40)
+--INSERT INTO CHITIETHANGVE (MaChuyenBay, MaHangVe, SoGhe) VALUES (2, 1, 20)
+--INSERT INTO CHITIETHANGVE (MaChuyenBay, MaHangVe, SoGhe) VALUES (2, 2, 40)
 
-INSERT INTO CHITIETHANGVE (MaChuyenBay, MaHangVe, SoGhe) VALUES (3, 2, 20)
-INSERT INTO CHITIETHANGVE (MaChuyenBay, MaHangVe, SoGhe) VALUES (3, 3, 90)
+--INSERT INTO CHITIETHANGVE (MaChuyenBay, MaHangVe, SoGhe) VALUES (3, 2, 20)
+--INSERT INTO CHITIETHANGVE (MaChuyenBay, MaHangVe, SoGhe) VALUES (3, 3, 90)
 
--- Nhập Khách hàng
-INSERT INTO KHACHHANG (HoTen, CMND, SDT, Email) VALUES (N'Nguyễn Văn Hên', '123456789', '0000000001', 'hen@mail.com')
-INSERT INTO KHACHHANG (HoTen, CMND, SDT, Email) VALUES (N'Phan Trường Huy', '135792460', '0000000002', 'huy@mail.com')
-INSERT INTO KHACHHANG (HoTen, CMND, SDT, Email) VALUES (N'Hàn Phi Trường', '2468135790', '0000000003', 'truong@mail.com')
-INSERT INTO KHACHHANG (HoTen, CMND, SDT, Email) VALUES (N'Trần Nguyễn Nhật Tân', '111222333', '0000000004', 'tan@mail.com')
-INSERT INTO KHACHHANG (HoTen, CMND, SDT, Email) VALUES (N'Nguyễn Đình Thi', '444555666', '0000000005', 'thi@mail.com')
+---- Nhập Khách hàng
+--INSERT INTO KHACHHANG (HoTen, CMND, SDT, Email) VALUES (N'Nguyễn Văn Hên', '123456789', '0000000001', 'hen@mail.com')
+--INSERT INTO KHACHHANG (HoTen, CMND, SDT, Email) VALUES (N'Phan Trường Huy', '135792460', '0000000002', 'huy@mail.com')
+--INSERT INTO KHACHHANG (HoTen, CMND, SDT, Email) VALUES (N'Hàn Phi Trường', '2468135790', '0000000003', 'truong@mail.com')
+--INSERT INTO KHACHHANG (HoTen, CMND, SDT, Email) VALUES (N'Trần Nguyễn Nhật Tân', '111222333', '0000000004', 'tan@mail.com')
+--INSERT INTO KHACHHANG (HoTen, CMND, SDT, Email) VALUES (N'Nguyễn Đình Thi', '444555666', '0000000005', 'thi@mail.com')
