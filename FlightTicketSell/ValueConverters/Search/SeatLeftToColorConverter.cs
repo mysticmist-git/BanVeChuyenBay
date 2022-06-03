4ffe2a74ad1db1e262b0c796851d1c23107d1d5f
Nguyen Van Hen <89691660+String1502@users.noreply.github.com>
2022-06-04 01:20:14 +0700

    [Tra cứu] - Hết ghế trống thì cho foreground màu đỏ

---
+++ FlightTicketSell/ValueConverters/Search/SeatLeftToColorConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Media;
+
+namespace FlightTicketSell.ValueConverters
+{
+    public class SeatLeftToColorConverter : BaseValueConverter<SeatLeftToColorConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int seatLeft;
+            var result = Int32.TryParse(value.ToString(), out seatLeft);
+
+            var converter = new BrushConverter();
+
+            // Error case
+            if (!result)
+                return (System.Windows.Media.Brush)converter.ConvertFromString("#0003ED");
+
+            // Success Case
+            if (seatLeft <= 0)
+                return (System.Windows.Media.Brush)converter.ConvertFromString("#C7282A");
+            else
+                return (System.Windows.Media.Brush)converter.ConvertFromString("#000000");
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
