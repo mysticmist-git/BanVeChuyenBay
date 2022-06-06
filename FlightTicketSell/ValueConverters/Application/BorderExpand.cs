114db23dc9aaf13f1e3817a98a45196346ebcb95
Gr33nBean <91081335+Gr33nBean@users.noreply.github.com>
2022-06-07 00:27:32 +0700

    Edit
    Edit 1 chút giao diện toàn App

---
+++ FlightTicketSell/ValueConverters/Application/BorderExpand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FlightTicketSell.ValueConverters
+{
+    public class BorderExpand : BaseValueConverter<BorderExpand>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new GridLength((double)value + 10);
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
